c92c57dc893abbfbb57b2d88ec433e361b1fa916
David Echoda <davidechoda123@gmail.com>
2021-09-13 21:18:36 +0000

    removing blank lines

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,5 @@ def no_c(my_string):
     for i in my_string:
         if i is not 'c' and i is not 'C':
             new_string += i
-    return new_string
+    return (new_string)
+# return my_string.translate({ord(c): None for c in "cC"})
