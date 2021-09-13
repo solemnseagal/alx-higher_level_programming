8a54326da6a65ed284b0809897f210b5e6f25223
David Echoda <davidechoda123@gmail.com>
2021-09-13 20:05:58 +0000

    correction to code

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    ret = my_list[:]
+    ret = []
+    for i in range(0, len(my_list)):
+        ret.append(my_list[i])
     if idx < 0 or idx > len(ret):
         return ret
     else:
