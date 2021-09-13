4d3c8b29e9bb81685b06ee5b8f6c0a86abdaf7a5
David Echoda <davidechoda123@gmail.com>
2021-09-13 20:07:53 +0000

    correction to checker

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    ret = []
-    for i in range(0, len(my_list)):
-        ret.append(my_list[i])
+    ret = my_list[:]
     if idx < 0 or idx > len(ret):
         return ret
     else:
