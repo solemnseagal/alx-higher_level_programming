2b2c73fe7cd3cf3236aa8b8bc7cdf03351a57c1a
David Echoda <davidechoda123@gmail.com>
2021-09-30 00:24:50 +0000

    adjusting code

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -86,3 +86,14 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
+
+    def __str__(self):
+        """String representation of a Square instance
+        Returns:
+            Formatted string representing the square
+        """
+        if self.size == 0:
+            return ""
+        string = "\n" * self.position[1] + (" " * self.position[0] +
+                                            "#" * self.size + "\n") * self.size
+        return string[:-1]
--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
+"""Defines a class MagicClass"""
 import math
 
 
 class MagicClass:
-    ''' Magic Class reverses given Class to bytecode'''
-
+    """This represents a circle"""
     def __init__(self, radius=0):
+        """Initializes the Magic Class"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        return self.__radius**2 * math.pi
+        """Calculaes the area of the circle"""
+        return (self.__radius ** 2) * math.pi
 
     def circumference(self):
+        """Calculates the circumference of the circle"""
         return 2 * math.pi * self.__radius
