72e8872171b88fa04a3901523c45df777786e847
Echoda David <davidechoda123@gmail.com>
2021-12-13 12:39:41 +0100

    removing extra lines

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const arg = process.argv;
-(arg.length > 3) ? console.log("Arguments found") : (arg.length == 3) ? console.log("Argument found") : console.log('No argument');
+(arg.length > 3) ? console.log('Arguments found') : (arg.length == 3) ? console.log('Argument found') : console.log('No argument');
