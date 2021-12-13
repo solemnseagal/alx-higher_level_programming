44ecc7ad965806f24e2786a59a0d73488c94f6f5
David Echoda <davidechoda123@gmail.com>
2021-12-13 23:59:17 +0000

    code refactor

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
-let A = Number(args[2]);
-let B = Number(args[3]);
+const A = Number(args[2]);
+const B = Number(args[3]);
 if (A !== null && B !== null) {
   console.log(add(A, B));
 } else {
