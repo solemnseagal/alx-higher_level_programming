8a04bdc57900ce25550a07b51985527110583e26
David Echoda <davidechoda123@gmail.com>
2021-12-13 22:59:33 +0000

    code refactor

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-
-['C is fun', 'Python is cool', 'Javascript is amazing'].forEach(
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'Javascript is amazing';
+[myVar1, myVar2, myVar3].forEach(
   el => console.log(el)
 );
