ef3461f04ad7cf1513340ab18fe78f424a38146e
Echoda David <davidechoda123@gmail.com>
2022-02-02 02:00:07 +0100

    code refactor

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,15 +4,17 @@ const url = 'https://swapi.co/api/films/' + process.argv[2];
 
 if (process.argv.length > 2) {
 request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
-  }
+if (error) {
+console.log(error)
+} 
+const characters = JSON.parse(body).characters;
+characters.forEach((character) => {
+request(character, function (error, response, body) {
+if (error) {
+console.log(error)
+}
+console.log(JSON.parse(body).name);
+});
+});
 });
 }
